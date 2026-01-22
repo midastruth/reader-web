41251cd491aca1e3ded52c3bb3a721ee3eaa05aa
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2026-01-22 15:55:38 +0100

    Correct supportedLngs
    Portuguese was not working properly because of regions

--- src/preferences/defaultPreferences.ts
+++ src/preferences/defaultPreferences.ts
@@ -36,7 +36,7 @@ import {
 
 export const defaultPreferences: ThPreferences<DefaultKeys> = createPreferences<DefaultKeys>({
 //  direction: ThLayoutDirection.ltr,
-  locale: "ta",
+//  locale: "en",
   experiments: {
     reflow: ["experimentalHeaderFiltering", "experimentalZoom"],
     webPub: ["experimentalHeaderFiltering", "experimentalZoom"]
