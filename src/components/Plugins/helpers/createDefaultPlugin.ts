a7a6efb9e01e55fc2132601cf16f33d03ef4e6cb
Jiminy Panoz <JayPanoz@users.noreply.github.com>
2026-03-09 10:28:42 +0100

    Bump version

--- src/components/Plugins/helpers/createDefaultPlugin.ts
+++ src/components/Plugins/helpers/createDefaultPlugin.ts
@@ -33,7 +33,7 @@ export const createDefaultPlugin = (): ThPlugin => {
     id: "core",
     name: "Core Components",
     description: "Default components for Thorium Web Epub StatefulReader",
-    version: "1.1.5",
+    version: "1.2.0",
     components: {
       actions: {
         [ThActionsKeys.fullscreen]: {
